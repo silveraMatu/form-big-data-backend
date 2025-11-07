128895bec11dc2a863fe54ceaf9bd88f586e6c38
Matute <matusilvera808@gmail.com>
2025-11-06 23:05:11 -0300

    cors configurado para recibir peticiones desde mi front en local

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import e from "express";
 import router from "./routes/registrarRespuesta.router.js";
+import cors from "cors"
 
 export const app = e()
 
 app.use(e.json())
+app.use(cors({
+  origin: 'http://localhost:3000' // <-- Cambia esto por tu URL de React
+}));
 app.use("/api", router)
